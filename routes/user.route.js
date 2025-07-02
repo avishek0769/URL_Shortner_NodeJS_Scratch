4f1bb77ecea9e663465449fe07d71da8d37ca504
avishek0769 <avishekadhikary42@gmail.com>
2025-07-02 12:37:25 +0530

    Create Routers and Middlewares from your own in Node.js

---
+++ routes/user.route.js
@@ -0,0 +1,31 @@
+import { createUser, handleSignupPage, loginUser, handleLoginPage } from "../controllers/user.controller.js";
+import { bodyParser } from "../middleware/bodyParser.js";
+
+
+const routes = [
+    { method: "GET", route: "/signup", middlewares: [], controller: handleSignupPage},
+    { method: "GET", route: "/login", middlewares: [], controller: handleLoginPage},
+    { method: "POST", route: "/signup", middlewares: [bodyParser], controller: createUser},
+    { method: "POST", route: "/login", middlewares: [], controller: loginUser},
+]
+
+export async function userRouter (req, res, initialRoute = "") {
+    for (const elem of routes) {
+        if(elem.method == req.method && (initialRoute + elem.route) == req.url){
+            let i = 0
+            let extraArgs = { url: req.url }
+            function next () {
+                if(i < elem.middlewares.length){
+                    if(res.writableEnded) return;
+                    else {
+                        elem.middlewares[i++](req, res, next, extraArgs);
+                    }
+                }
+                else {
+                    elem.controller(req, res)
+                }
+            }
+            next()
+        }
+    }
+}

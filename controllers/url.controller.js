6257ad6b6353c0fbaa655341458939abf91fb965
avishek0769 <avishekadhikary42@gmail.com>
2025-07-02 18:43:52 +0530

    Create functionality of fetching and creation of Urls

--- controllers/url.controller.js
+++ controllers/url.controller.js
@@ -1,12 +1,46 @@
 import { Url } from "../models/url.models.js"
+import fs from "fs"
+import ejs from "ejs"
+import { projectDir } from "../index.js"
+import shortid from "shortid"
+import {UAParser} from "ua-parser-js"
 
 
-const createShortUrl = (req, res) => {
-    
+const createShortUrl = async (req, res) => {
+    console.log(req.body)
+    const { originalUrl } = req.body
+    const userId = req.user._id
+
+    const urlShortId = shortid.generate()
+    const parser = new UAParser()
+    const result = parser.setUA(req.headers["user-agent"]).getResult()
+
+    const urlDoc = await Url.create({
+        createdBy: userId,
+        shortId: urlShortId,
+        redirectUrl: originalUrl,
+        historyClicked: {
+            timestamp: Date.now(),
+            ip: req.socket.remoteAddress,
+            device: result.os
+        }
+    })
+    console.log(urlDoc)
+    res.writeHead(302, { "Location": "/url" })
+    res.end()
 }
 
-const handleHomePage = (req, res) => {
-    
+const handleHomePage = async (req, res) => {
+    if (!req.user) {
+        res.writeHead(301, { "Location": "http://localhost:3000/users/login" })
+        return res.end()
+    }
+
+    const urlDocs = await Url.find({ createdBy: req.user._id })
+    fs.readFile(`${projectDir}/views/home.ejs`, "utf-8", (err, template) => {
+        const html = ejs.render(template, { urlDocs })
+        res.end(html)
+    })
 }
 
 export {

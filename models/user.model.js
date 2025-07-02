1269e03793643cbf6043ae0f8e8d8da79782514c
avishek0769 <avishekadhikary42@gmail.com>
2025-07-02 12:35:45 +0530

    Create user model / schema

---
+++ models/user.model.js
@@ -0,0 +1,19 @@
+import mongoose, { Schema } from "mongoose";
+
+
+const userSchema = new Schema({
+    fullname: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+})
+
+export const User = mongoose.model("User", userSchema)

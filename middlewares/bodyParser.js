9143b0328b7485fbfa3106135b6f6851a632f886
avishek0769 <avishekadhikary42@gmail.com>
2025-07-02 12:39:05 +0530

    Create Middleware function --> Body parser for parsing raw data into req.body

---
+++ middlewares/bodyParser.js
@@ -0,0 +1,23 @@
+
+export const bodyParser = (req, res, next) => {
+    let rawData = ""
+    req.on("data", (chunk) => {
+        rawData += chunk
+    })
+    req.on("end", () => {
+        if(req.headers["content-type"] == "application/x-www-form-urlencoded") {
+            let body = {};
+            rawData.split("&").forEach(pair => {
+                let splitedValue = pair.split("=")
+                let key = splitedValue[0]
+                let value = splitedValue[1].replace("%40", "@").replace("%20", " ")
+                body = { ...body, [key]: value }
+            })
+            req.body = body
+        }
+        else if(req.headers["content-type"] == "application/json") {
+            req.body = JSON.parse(rawData)
+        }
+        next()
+    })
+}

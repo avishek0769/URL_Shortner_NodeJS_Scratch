70a092b0d9df0f1655e387beeeb0c2c866f0d147
avishek0769 <avishekadhikary42@gmail.com>
2025-07-02 18:44:26 +0530

    Create Auth middleware handler

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,5 +1,21 @@
+import jwt from "jsonwebtoken";
 
-export const auth = (req, res) => {
-    const token = req.headers
-    console.log(token)
+export const auth = (req, res, next) => {
+    const token = req.headers['cookie']
+    if(!token) {
+        req.user = null
+        return next()
+    }
+    let parsedCookieObject = {}
+
+    token.split("&").forEach(pair => {
+        const splitedValue = pair.split("=")
+        const key = splitedValue[0]
+        const value = splitedValue[1]
+        parsedCookieObject = { ...parsedCookieObject, [key]: value }
+    });
+    req.cookies = parsedCookieObject
+    const decodedUser = jwt.verify(parsedCookieObject.authToken, "secret")
+    req.user = decodedUser
+    next()
 }
